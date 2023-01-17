dfeb06d410ce4f83a4f0ffbf25955df04465cd8f
Sinthella Anpalagan <sinthella1@gmail.com>
2023-01-17 10:04:03 +0100

    createdt ImageView and TextView and UI and initialised variables

--- app/src/main/java/sinthella/anpalagan/weatherapp/MainActivity.java
+++ app/src/main/java/sinthella/anpalagan/weatherapp/MainActivity.java
@@ -1,14 +1,41 @@
 package sinthella.anpalagan.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RelativeLayout homeRL;
+    private ProgressBar loadingPB;
+    private TextView cityNameTV,temperatureTV, conditionTV;
+    private RecyclerView weatherRV;
+    private TextInputEditText cityEdt;
+    private ImageView backIV,iconIV,searchIV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        homeRL=findViewById(R.id.idRLHome);
+        loadingPB=findViewById(R.id.idPBLoading);
+        cityNameTV=findViewById(R.id.idTVCityName);
+        conditionTV=findViewById(R.id.idTVCondition);
+        weatherRV=findViewById(R.id.idRvWeather);
+        cityEdt=findViewById(R.id.idEdtCity);
+        backIV=findViewById(R.id.idIVBack);
+        iconIV=findViewById(R.id.idTVIcon);
+        searchIV=findViewById(R.id.idIVSearch);
+
+
+
+
     }
 }
