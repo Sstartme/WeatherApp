ea616df8d03e74a3e542bc3c822aadeb2b163eba
Sinthella Anpalagan <sinthella1@gmail.com>
2023-01-18 14:28:53 +0100

    use to make my App full screen so we do not see my statusbar app

--- app/src/main/java/sinthella/anpalagan/weatherapp/MainActivity.java
+++ app/src/main/java/sinthella/anpalagan/weatherapp/MainActivity.java
@@ -6,12 +6,15 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
 
 public class MainActivity<linearLayoutManager> extends AppCompatActivity {
 
@@ -22,12 +25,15 @@ public class MainActivity<linearLayoutManager> extends AppCompatActivity {
     private TextInputEditText cityEdt;
     private ImageView backIV,iconIV,searchIV;
     private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<WeatherRVModal> weatherRVModalArrayList;
+    private WeatherRVAdapter weatherRVAdapter;
 
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         setContentView(R.layout.activity_main);
         homeRL=findViewById(R.id.idRLHome);
         loadingPB=findViewById(R.id.idPBLoading);
@@ -38,9 +44,12 @@ public class MainActivity<linearLayoutManager> extends AppCompatActivity {
         backIV=findViewById(R.id.idIVBack);
         iconIV=findViewById(R.id.idTVIcon);
         searchIV=findViewById(R.id.idIVSearch);
-
+        weatherRVModalArrayList = new ArrayList<>();
+        weatherRVAdapter = new WeatherRVAdapter(this,weatherRVModalArrayList);
+        weatherRV.setAdapter(weatherRVAdapter);
     }
+    private void  getWeatherInfo(String cityName){
+            String url = "http://api.weatherapi.com/v1/forecast.json?key=8ca4db650e9842c9ab390658231701&q="+cityName+"&days=1&aqi=no&alerts=no";
 
-    private class lateinit {
     }
 }
