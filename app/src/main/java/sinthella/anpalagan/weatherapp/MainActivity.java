a8cf2e5c1fb28cf81bce532821229c3c6291a0e2
Sinthella Anpalagan <sinthella1@gmail.com>
2023-01-18 15:35:31 +0100

    added Methode to get the CityName

--- app/src/main/java/sinthella/anpalagan/weatherapp/MainActivity.java
+++ app/src/main/java/sinthella/anpalagan/weatherapp/MainActivity.java
@@ -1,20 +1,32 @@
 package sinthella.anpalagan.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity<linearLayoutManager> extends AppCompatActivity {
 
@@ -27,26 +39,60 @@ public class MainActivity<linearLayoutManager> extends AppCompatActivity {
     private RecyclerView.LayoutManager layoutManager;
     private ArrayList<WeatherRVModal> weatherRVModalArrayList;
     private WeatherRVAdapter weatherRVAdapter;
+    private LocationManager locationManager;
+    private int PERMISSION_CODE=1;
+
 
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         setContentView(R.layout.activity_main);
-        homeRL=findViewById(R.id.idRLHome);
-        loadingPB=findViewById(R.id.idPBLoading);
-        cityNameTV=findViewById(R.id.idTVCityName);
-        conditionTV=findViewById(R.id.idTVCondition);
-        weatherRV=findViewById(R.id.idRvWeather);
-        cityEdt=findViewById(R.id.idEdtCity);
-        backIV=findViewById(R.id.idIVBack);
-        iconIV=findViewById(R.id.idTVIcon);
-        searchIV=findViewById(R.id.idIVSearch);
+        homeRL = findViewById(R.id.idRLHome);
+        loadingPB = findViewById(R.id.idPBLoading);
+        cityNameTV = findViewById(R.id.idTVCityName);
+        conditionTV = findViewById(R.id.idTVCondition);
+        weatherRV = findViewById(R.id.idRvWeather);
+        cityEdt = findViewById(R.id.idEdtCity);
+        backIV = findViewById(R.id.idIVBack);
+        iconIV = findViewById(R.id.idTVIcon);
+        searchIV = findViewById(R.id.idIVSearch);
         weatherRVModalArrayList = new ArrayList<>();
-        weatherRVAdapter = new WeatherRVAdapter(this,weatherRVModalArrayList);
+        weatherRVAdapter = new WeatherRVAdapter(this, weatherRVModalArrayList);
         weatherRV.setAdapter(weatherRVAdapter);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity, this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_CODE);
+        }
+        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+    }
+
+        private String getCityName(double longitude, double latitude){
+        String cityName = "Not found";
+            Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
+            try {
+                List<Address> addresses = gcd.getFromLocation(latitude, longitude, 10);
+                for (Address adr: addresses){
+                    if(adr!=null){
+                        String city = adr.getLocality();
+                        if(city!=null && !city.equals("")){
+                            cityName = city;
+                        } else {
+                            Log.d("TAG","CITY NOT FOUND");
+                            Toast.makeText(this, "User City Not Found...",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }
+
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+            return cityName;
+
+
     }
     private void  getWeatherInfo(String cityName){
             String url = "http://api.weatherapi.com/v1/forecast.json?key=8ca4db650e9842c9ab390658231701&q="+cityName+"&days=1&aqi=no&alerts=no";
